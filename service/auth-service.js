bec8c70436d41f2306df69e9ae110f9fb65a9f05
alyuza <alyuza.sat@gmail.com>
2023-08-28 14:11:24 +0700

    add auth-service.js

---
+++ service/auth-service.js
@@ -0,0 +1,60 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { JWT_SIGN } = require('../config/jwt.js')
+
+// ========================== REGISTER =========================
+const validRoles = ["user"]
+const register = async (req, res) => {
+    const { username, password, role } = req.body
+    try {
+        if (!username || username.trim() === "" || !/^[a-zA-Z0-9.]+$/.test(username)) {
+            res.status(400).json({ message: "Username can't be blank and doesn't allow to enter of any special character except dot (.)" });
+            return;
+        }
+        if (!password || password.length < 8) {
+            res.status(400).json({ message: "Password must be at least 8 characters long" });
+            return;
+        }
+        if (!validRoles.includes(role)) {
+            throw new Error("Invalid role, can register with role 'user' only")
+        }
+
+        const user = await req.db.collection('users').findOne({ username })
+        if (user) {
+            throw new Error('Sorry, username already exists')
+        }
+        const hashedPassword = await bcrypt.hash(password, 10) //hashedpass : password yg sudah di encrypted
+        const newUser = await req.db.collection('users').insertOne({ username, password: hashedPassword, role })
+        res.status(200).json({
+            message: `User ${username} successfully registered`,
+            data: newUser,
+        })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+// ========================== LOGIN =========================
+const login = async (req, res) => {
+    const { username, password } = req.body
+    const user = await req.db.collection('users').findOne({ username })
+    if (!user) {
+        res.status(400).json({ error: 'Username is not registered' });
+        return;
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+    if (isPasswordCorrect) {
+        const token = jwt.sign({ username: user.username, role: user.role }, JWT_SIGN)
+        res.status(200).json({
+            message: 'Successfully logged in',
+            data: token
+        });
+    } else {
+        res.status(400).json({ error: 'Password is incorrect' });
+    }
+}
+
+module.exports = {
+    register,
+    login
+}
