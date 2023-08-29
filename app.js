0d22024ac67b4ba542e3ab3d21613c44e056578c
alyuza <alyuza.sat@gmail.com>
2023-08-29 18:41:17 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 require('dotenv').config();
-const express = require('express')
+const express = require('express');
 const app = express();
-const authRouter = require('./routes/auth-router.js');
-const databaseMiddleware = require('./middleware/database-middleware');
-const authenticationMiddleware = require('./middleware/authentication-middleware.js')
-const { login, register } = require('./service/auth-service.js');
-const { adminAuthMiddleware } = require('./middleware/authorization-middleware.js');
 const port = process.env.PORT || 8080;
+const databaseMiddleware = require('./middleware/database-middleware');
+const authRouter = require('./routes/auth-router.js');
+const { adminRouter } = require('./routes/admin-router.js');
+const { userRouter } = require('./routes/user-router.js');
 
 app.use(express.json());
 app.use(databaseMiddleware);
 
-app.get('/', (req, res) => {res.send('PROJECT MILESTONE 2 - ALYUZA SATRIO PRAYOGO')})
-app.use('/auth', authRouter, login)
-app.use('/admin', authenticationMiddleware, adminAuthMiddleware, register)
+app.get('/', (req, res) => { res.send('PROJECT MILESTONE 2 - ALYUZA SATRIO PRAYOGO') });
+
+app.use('/v1/auth', authRouter);
+app.use('/v1/admin', adminRouter);
+app.use('/v1/user', userRouter);
 
 app.listen(port, () => {
-    console.log(`Running on localhost: ${port}`)
+    console.log(`Running on localhost: ${port}`);
 })
