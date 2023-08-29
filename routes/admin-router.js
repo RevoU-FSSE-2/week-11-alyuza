a2428cc28afbe9622b7f4153ade0d7baa2d37278
alyuza <alyuza.sat@gmail.com>
2023-08-29 23:24:48 +0700

    modify admin-router.js

--- routes/admin-router.js
+++ routes/admin-router.js
@@ -3,9 +3,11 @@ const adminRouter = Router();
 const { getAllUsers, getUser, updateUser, deleteUser } = require('../service/admin-service');
 const authenticationMiddleware = require('../middleware/authentication-middleware');
 const { adminAuthMiddleware } = require('../middleware/authorization-middleware');
+const { register } = require('../service/auth-service');
 
 adminRouter.get('/getAllUsers', authenticationMiddleware, adminAuthMiddleware, getAllUsers)
 adminRouter.get('/getUser/:id', authenticationMiddleware, adminAuthMiddleware, getUser)
+adminRouter.post('/register', authenticationMiddleware, adminAuthMiddleware, register)
 adminRouter.put('/update/:id', authenticationMiddleware, adminAuthMiddleware, updateUser)
 adminRouter.delete('/delete/:id', authenticationMiddleware, adminAuthMiddleware, deleteUser)
 
