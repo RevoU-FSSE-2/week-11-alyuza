158967e22afe709d93d826def6398ff2ff4caafd
alyuza <alyuza.sat@gmail.com>
2023-08-29 23:24:41 +0700

    modify auth-router.js

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,10 +1,7 @@
 const { Router } = require('express');
 const authRouter = Router();
-const { register, login } = require('../service/auth-service.js');
-const authenticationMiddleware = require('../middleware/authentication-middleware.js');
-const { adminAuthMiddleware } = require('../middleware/authorization-middleware.js');
+const { login } = require('../service/auth-service.js');
 
 authRouter.post('/login', login);
-authRouter.post('/register', authenticationMiddleware, adminAuthMiddleware, register);
 
 module.exports = authRouter;
