e6a85afe1aa0d433214330257109cc03d507c417
alyuza <alyuza.sat@gmail.com>
2023-08-30 13:43:47 +0700

    modify user-router.js

--- routes/user-router.js
+++ routes/user-router.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
 const { userAuthMiddleware } = require('../middleware/authorization-middleware');
-const authenticationMiddleware = require('../middleware/authentication-middleware');
 const { changePassword, viewProfile } = require('../service/user-service');
 const userRouter = Router();
 
-userRouter.get('/viewProfile/:id', authenticationMiddleware, userAuthMiddleware, viewProfile)
-userRouter.patch('/changePassword/:id', authenticationMiddleware, userAuthMiddleware, changePassword)
+userRouter.get('/viewProfile/:id', userAuthMiddleware, viewProfile)
+userRouter.patch('/changePassword/:id', userAuthMiddleware, changePassword)
 
 module.exports = { userRouter }
