f27af5c2e4ed7a5b3883412f926f820d2380a82d
alyuza <alyuza.sat@gmail.com>
2023-09-01 01:51:33 +0700

    modify user-service.js

--- service/user-service.js
+++ service/user-service.js
@@ -1,22 +1,45 @@
-const { ObjectId } = require("mongodb");
 const bcrypt = require('bcrypt');
 
-// Change user's password
-const changePassword = async (req, res) => {
-  const id = req.params.id;
-  const { password } = req.body;
+// View profile
+const viewProfile = async (req, res) => {
+  const usernameInput = res.locals.username; // Automatic get data from decoded JWT token
   try {
-    const user = await db.collection('users').findOne({ _id: new ObjectId(id) });
+    const user = await req.db.collection('users').findOne({
+      username: usernameInput,
+      is_deleted: { $exists: false }
+    });
+    if (user) {
+      return res.status(200).json({ message: `Success get own profile.`, data: user });
+    } else {
+      return res.status(404).json({ message: `User not found or deleted.` });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+// Change password
+const changePassword = async (req, res) => {
+  const usernameInput = res.locals.username; // Automatic get data from decoded JWT token
+  const { password, repeatPassword } = req.body;
+  try {
+    const user = await db.collection('users').findOne({
+      username: usernameInput,
+      is_deleted: { $exists: false }
+    });
     if (!user) {
-      return res.status(404).json({ message: `User with ID ${id} not found.` });
+      return res.status(404).json({ message: `User with ID not found.` });
     }
     if (!password || password.length < 8) {
-      res.status(400).json({ message: "Password must be at least 8 characters long" });
+      res.status(400).json({ message: "Password must be at least 8 characters long." });
       return;
+    }
+    if (password !== repeatPassword) {
+      return res.status(400).json({ message: 'Passwords do not match.' });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const passwordUpdate = await db.collection('users').updateOne(
-      { _id: new ObjectId(id) },
+      { username: usernameInput },
       { $set: { password: hashedPassword } }
     );
     res.status(200).json({
@@ -27,29 +50,6 @@ const changePassword = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
-
-// View profile
-const viewProfile = async (req, res) => {
-  try {
-    const userID = req.params.id; // Extract user ID from the request parameters
-    const user = await req.db.collection('users').findOne({
-      _id: new ObjectId(userID),
-      is_deleted: { $exists: false }
-    });
-    if (user) {
-      res.status(200).json({
-        message: `Success get user's data`,
-        data: user
-      });
-    } else {
-      res.status(404).json({
-        message: `User with ID: ${userID} not found`
-      });
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-}
 
 module.exports = {
   changePassword,
