efdea56f1c0806d6e3b67066decfbedb45cd4772
alyuza <alyuza.sat@gmail.com>
2023-09-01 01:51:41 +0700

    modify auth-service.js

--- service/auth-service.js
+++ service/auth-service.js
@@ -2,12 +2,12 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { JWT_SIGN } = require('../config/jwt.js');
 
-// ========================== LOGIN =========================
+// Login
 const login = async (req, res) => {
   const { username, password } = req.body;
   const user = await req.db.collection('users').findOne({ username });
   if (!user) {
-    res.status(400).json({ error: 'Username is not registered' });
+    res.status(400).json({ error: 'Username is not registered.' });
     return;
   }
   const isPasswordCorrect = await bcrypt.compare(password, user.password);
@@ -18,7 +18,7 @@ const login = async (req, res) => {
       data: token
     });
   } else {
-    res.status(400).json({ error: 'Password is incorrect' });
+    res.status(400).json({ error: 'Password is incorrect.' });
   }
 }
 
