b5cb0bac99b9dbd3fa5fd6f2b9c5329cb553cdbf
alyuza <alyuza.sat@gmail.com>
2023-09-01 01:52:06 +0700

    modify auth-router.js

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
-const authRouter = Router();
 const { login } = require('../service/auth-service.js');
+const authRouter = Router();
 
 authRouter.post('/login', login);
 
